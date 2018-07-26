b733b16769debd516cfe2eaa1ad993b7bdb85772
EspeuteClement <EspeuteClement@github.com>
2018-07-26 13:34:56 +0200

    Better build.bat

--- build.bat
+++ build.bat
@@ -2,15 +2,30 @@
 
 echo %time%
 
+set GAME_NAME = win32_platform
+
+rem === Common Stuff ===
+set COMMON_FLAGS=/Zi /INCREMENTAL:NO -FC /nologo
+set COMMON_LINK=/INCREMENTAL:NO
+
+rem == DLL Config ===
+set DLL_SOURCES=../src/game.all.cpp
+set DLL_FLAGS=%COMMON_FLAGS% /Fe:game.dll /DLL /LDd
+
+rem === Host Config ===
 set LIBS=Gdi32.lib kernel32.lib user32.lib Shell32.lib Winmm.lib
-set FLAGS=/Zi /INCREMENTAL:NO -FC 
+set EXE_NAME=%GAME_NAME%.exe
 
 IF NOT EXIST %~dp0build mkdir %~dp0build
 pushd %~dp0build
+IF NOT EXIST tmp mkdir tmp
+
 del /Q *.pdb > NUL 2> NUL
-cl  /Fe:game.dll %FLAGS% ../src/game.all.cpp /DLL /LDd /nologo /link /INCREMENTAL:NO /PDB:game.%random%.pdb
-echo === Finished building game.dll at %time%
-cl  %FLAGS%  /D_GLFW_WIN32 ../src/win32_platform.cpp ../src/external/external.all.c %LIBS%  /nologo /link /INCREMENTAL:NO /PDB:testglfw.%random%.pdb
+@echo on
+cl %DLL_FLAGS% %DLL_SOURCES% /link %COMMON_LINK% /PDB:game.%random%.pdb
+@echo === Finished building game.dll at %time%
+cl /Fe%EXE_NAME% %COMMON_FLAGS%  /D_GLFW_WIN32 ../src/win32_platform.cpp ../src/external/external.all.c %LIBS% /link %COMMON_LINK% /PDB:%GAME_NAME%.%random%.pdb
+@echo off
 echo %time%
 popd
 
